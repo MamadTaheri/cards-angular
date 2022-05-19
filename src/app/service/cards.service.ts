998419dd1939fe7e8df13824611b4122385f4e5e
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-19 21:43:39 +0430

    create model and service

---
+++ src/app/service/cards.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Card } from '../models/card.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardsService {
+
+  baseUrl = 'https://localhost:7271/api/Cards'
+
+  constructor(private http: HttpClient) { }
+
+  // Get all cards
+  getAllCards(): Observable<Card[]>{
+    return this.http.get<Card[]>(this.baseUrl);
+  }
+}
